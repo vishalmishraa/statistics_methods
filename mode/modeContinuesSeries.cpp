d402b951b60feb4f296d27b9f4e2f2f5653c4b62
vishalmishraa <vishal.mishra9470@gmail.com>
2021-03-14 10:49:46 +0530

    adding sum unrealted codes

--- mode/modeContinuesSeries.cpp
+++ mode/modeContinuesSeries.cpp
@@ -3,36 +3,40 @@
 #include "algorithm"
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the size of the Data :"<<endl;
-    cin>>n;
+    cout << "Enter the size of the Data :" << endl;
+    cin >> n;
 
-    float a[n+1],b[n+1],f[n+1];
-            a[0]=0;b[0]=0;f[0]=0;
+    float a[n + 1], b[n + 1], f[n + 1];
+    a[0] = 0;
+    b[0] = 0;
+    f[0] = 0;
+    cout << "class\t"
+         << "frequency\t" << endl;
 
-    cout<<"class\t"<<"frequency\t"<<endl;
-
-    for(int i=1;i<=n;i++){
-        cin>>a[i]>>b[i]>>f[i];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i] >> b[i] >> f[i];
     }
 
     //Now find the most frequent value
-    float mf = *max_element(f,f+n);
-    int index = distance(f,find(f,f+n,mf));
-    
+    float mf = *max_element(f, f + n);
+    int index = distance(f, find(f, f + n, mf));
+
     //now implement the formula
-    float mode =0,l=0,f0=0,f1=0,f2=0,h=0;
-    
-    l=a[index];
-    h=b[index]-a[index];
-    f1=f[index];
-    f0=f[index-1];
-    f2=f[index+1];
-    
-    mode = l + (((f1-f0)/(2*f1 - f0 -f2))*h);
-    
-    cout<<"Mode of a continues series is : "<<mode<<endl;
+    float mode = 0, l = 0, f0 = 0, f1 = 0, f2 = 0, h = 0;
+
+    l = a[index];
+    h = b[index] - a[index];
+    f1 = f[index];
+    f0 = f[index - 1];
+    f2 = f[index + 1];
+
+    mode = l + (((f1 - f0) / (2 * f1 - f0 - f2)) * h);
+
+    cout << "Mode of a continues series is : " << mode << endl;
 
     return 0;
 }
